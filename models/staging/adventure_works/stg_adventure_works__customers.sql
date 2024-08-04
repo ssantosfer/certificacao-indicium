c168ac5a02ad65c0fb631d6af12fd130f947f336
Fernanda Silva <santosilvafernanda123@gmail.com>
2024-08-04 22:20:53 +0000

    Created intermediate models

--- models/staging/adventure_works/stg_adventure_works__customers.sql
+++ models/staging/adventure_works/stg_adventure_works__customers.sql
@@ -2,7 +2,7 @@ with
     transform as (
         select
             cast(customerid as int) as pk_cliente
-            , cast(personid as int) as fk_vendedor
+            , cast(personid as int) as fk_pessoa
             , cast(storeid as int) as fk_loja
             , cast(territoryid as int) as fk_territorio
         from {{ source('raw_adventure_works', 'customer') }}
--- models/staging/adventure_works/stg_adventure_works__products.sql
+++ models/staging/adventure_works/stg_adventure_works__products.sql
@@ -2,9 +2,9 @@ with
     transform as (
         select
             cast(productid as int) as pk_produto
-            , cast(name as varchar) as nome_produto
-            , cast(productnumber as varchar) as numero_produto
             , cast(productsubcategoryid as int) as fk_sub_categoria
+            , cast(productnumber as varchar) as numero_produto
+            , cast(name as varchar) as nome_produto
         from {{ source('raw_adventure_works', 'product') }}
     )
 select *
